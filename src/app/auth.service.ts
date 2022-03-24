19ad241f5db446ff4e6452ffb0023e45fce04b26
gokul <gokulchandan18@gmail.com>
2022-03-24 20:52:32 +0530

    commit

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Router } from '@angular/router'
+import { Observable, BehaviorSubject } from 'rxjs';
+import { User } from './user';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
 
   private _registerUrl = "http://localhost:3000/api/register";
   private _loginUrl = "http://localhost:3000/api/login";
+  public currentUser: Observable<User>;
+  private currentUserSubject: BehaviorSubject<User>;
 
   constructor(private http: HttpClient,
-              private _router: Router) { }
+              private _router: Router) {
+                this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+                this.currentUser = this.currentUserSubject.asObservable();
+               }
+
+               public get currentUserValue(): User {
+                return this.currentUserSubject.value;
+            }
 
   registerUser(user: {}) {
     return this.http.post<any>(this._registerUrl, user)
@@ -17,11 +29,19 @@ export class AuthService {
 
   loginUser(user: {}) {
     return this.http.post<any>(this._loginUrl, user)
+
+    .pipe(map(user => {
+      // store user details and jwt token in local storage to keep user logged in between page refreshes
+      localStorage.setItem('currentUser', JSON.stringify(user));
+      this.currentUserSubject.next(user);
+      return user;
+  }));
   }
 
   logoutUser() {
     localStorage.removeItem('token')
     this._router.navigate(['/events'])
+    this.currentUserSubject.next(null);
   }
 
   getToken() {
